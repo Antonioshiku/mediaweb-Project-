4a18b97fe8de05ca4f87e3e24b305c4da67fbb00
joekudo <joekudo303@gmail.com>
2022-06-20 16:59:20 +0630

    media_website

--- media-website/js/script.js
+++ media-website/js/script.js
@@ -7,21 +7,6 @@ const PhoneMenu = document.querySelector(".cate");
 
 window.addEventListener("resize", () => {
   // console.log(typeof window.innerWidth);
-
-  if (window.innerWidth < 912) {
-    container.style.display = "none";
-    mainNavCon.style.display = "none";
-  } else {
-    container.style.display = "grid";
-    mainNavCon.style.display = "block";
-  }
-
-  if (window.innerWidth > 921 && window.innerWidth < 1168) {
-    console.log(subCon);
-    subCon.classList.add("change_width");
-  } else {
-    subCon.classList.remove("change_width");
-  }
 });
 
 dropList.onclick = () => {
@@ -34,7 +19,7 @@ dropList.onclick = () => {
   }
 };
 
-menuCon.addEventListener("click", () => {
+menuCon.onclick= () => {
   menuCon.classList.add(".change_menu_bar");
 
   if (menuCon.classList.contains("change_menu_bar")) {
@@ -44,17 +29,20 @@ menuCon.addEventListener("click", () => {
   }
 
   const phoneNavBar = menuCon.parentElement.nextElementSibling;
-  //  console.log(phoneNavBar);
-  phoneNavBar.classList.add(".show_nav_bar");
+   console.log(phoneNavBar);
+      phoneNavBar.classList.add('.show_nav_bar');
 
-  if (phoneNavBar.classList.contains("show_nav_bar")) {
-    phoneNavBar.classList.remove("show_nav_bar");
-    phoneNavBar.classList.add("hide_nav_bar");
-  } else {
-    phoneNavBar.classList.add("show_nav_bar");
-    phoneNavBar.classList.remove("hide_nav_bar");
-  }
-});
+      const contains=phoneNavBar.classList.contains('show_nav_bar');
+
+      if (contains) {
+        phoneNavBar.classList.remove('show_nav_bar');
+        phoneNavBar.classList.add("hide_nav_bar")
+      } else {
+        phoneNavBar.classList.add('show_nav_bar');
+        phoneNavBar.classList.remove("hide_nav_bar")
+
+      }
+};
 
 PhoneMenu.onclick = () => {
   let phoneDropDown = PhoneMenu.nextElementSibling;
